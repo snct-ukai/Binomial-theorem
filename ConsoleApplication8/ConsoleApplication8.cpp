9b06a46374c71c74886f824d1afd41cd9a2fdadc
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2021-01-31 21:36:59 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+using namespace std;
+
+int kaijo(int a) {
+	int x = 1;
+	for (int i = 1; i <= a; i++) {
+		x *= i;
+	}
+	return x;
+}
+
+int main(void) {
+	int s;
+	cin >> s;
+	const int r = s;
+	int a = 0;
+	int b = 0;
+	for (int i = 0; i <= r; i++) {
+		cout << kaijo(r) / kaijo(i) / kaijo(r - i) << "x^" << r - 1 << " y^" << i ;
+		if (i != r) {
+			cout <<" + ";
+		}
+	}
+}
